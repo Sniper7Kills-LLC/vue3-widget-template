bf5e467f0145672f149fd09abfa5b21bce9224ca
Will G <sniper7kills@users.noreply.github.com>
2024-01-14 12:32:23 -0500

    Update POC to use gist for TestWidget

--- playground/App.vue
+++ playground/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="resolvedComponent"></component>
+  <component :is="resolvedComponent" v-bind="settings"></component>
 </template>
 
 <script lang="ts" setup>
@@ -8,25 +8,26 @@ import { ref, onMounted } from "vue";
 // Define a ref to hold the resolved component
 const resolvedComponent = ref(null);
 
+const settings = ref({ message: "Custom Message" });
+
 onMounted(async () => {
   try {
     // Call the externalComponent function to load the component
-    const component = await externalComponent("TestWidget.umd.min.js");
+    const component = await externalComponent(
+      "https://gist.githack.com/sniper7kills/6b42ba3125724c5709f604e90379e18f/raw/0f0a9c529cbfb00688eee0546958a106c036273c/TestWidget.umd.min.js"
+    );
     resolvedComponent.value = component;
   } catch (error) {
-    console.error(error);
+    console.error("Error loading external component:", error);
   }
 });
 
 // External component loading function
-async function externalComponent(url: string) {
-  console.log(url);
-  const name = url
-    .split("/")
-    .reverse()[0]
-    .match(/^(.*?)\.umd/)[1];
-
+async function loadScript(url: string, name: string) {
   if (window[name]) return window[name];
+
+  const timestamp = new Date().getTime();
+  const scriptUrl = `${url}?t=${timestamp}`;
 
   return new Promise((resolve, reject) => {
     const script = document.createElement("script");
@@ -35,12 +36,27 @@ async function externalComponent(url: string) {
       resolve(window[name]);
     });
     script.addEventListener("error", () => {
-      reject(new Error(`Error loading ${url}`));
+      reject(new Error(`Error loading ${scriptUrl}`));
     });
 
-    script.src = url;
+    script.src = scriptUrl;
 
     document.head.appendChild(script);
   });
+}
+
+async function externalComponent(url: string) {
+  const matchResult = url
+    .split("/")
+    .reverse()[0]
+    .match(/^(.*?)\.umd/);
+  const name = matchResult ? matchResult[1] : null;
+
+  if (!name) {
+    console.error(`Error: Unable to extract name from URL ${url}`);
+    return null;
+  }
+
+  return loadScript(url, name);
 }
 </script>
